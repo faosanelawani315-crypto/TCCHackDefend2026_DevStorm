3a8df40a19b0f4efa47e9030a2ef27bbc3a09298
lonehuntress <lonehuntres@gmail.com>
2026-06-06 13:53:49 +0000

    h

--- backend/app.py
+++ backend/app.py
@@ -74,3 +74,15 @@ def predict(
         "probability":
             probability
     }
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
